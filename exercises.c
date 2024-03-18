2558b383b1e0b7f0bf0629c926adfaaa8d479c70
Your Name <you@example.com>
2024-03-18 16:41:55 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -118,6 +118,12 @@ void copia_pila(Stack *P1, Stack *P2) {
   int *elem2 = top(pilaAux);
   printf("PRIMERO AUX: %i", (*elem2));
 
+  while(elem2 != NULL) {
+    push(P2, elem2);
+    pop(pilaAux);
+    elem = top(P2);
+  }
+
   
 
 }
